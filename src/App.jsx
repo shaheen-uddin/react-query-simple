4bd31de5cc99bdaa729d032e425a5938da710a68
shaheen-uddin <shaheenelt@gmail.com>
2024-02-28 18:39:17 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+     <h2 className="bg-orange-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus pariatur sed enim!</h2>
+    </>
+  )
+}
+
+export default App
